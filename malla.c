3cb4b21f14ee2783ab18cf18b8483c58129833db
jmcm39-ua <98743487+jmcm39-ua@users.noreply.github.com>
2022-10-04 10:52:02 +0200

    Add files via upload

---
+++ malla.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    pid_t malla;
+    int x = 0;
+    int y = 0;
+    int contx = 0;
+    printf("Inserte el valor X: ");
+    scanf("%d",&x);
+    printf("INserte el valor Y: ");
+    scanf("%d",&y);
+    /*pid_t nombres[x][y];
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < y; j++){
+            if (malla>0){
+            nombres[i][j] = fork();
+            sleep(10);
+            }
+        }
+    }*/
+
+    int ppid = getpid();
+    malla = fork();
+
+    for(int i = 0; i < y - 1;i++){
+
+        if (getpid() == ppid && malla > 0 ){
+            fork();
+
+        }
+
+        
+    }
+
+    sleep(20);
+
+    
+    
+
+}
