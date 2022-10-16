63b6629c3b2f8ea82781140ac3102fa5e0ba3edf
monica <monica@pop-os.localdomain>
2022-10-16 12:22:29 +0200

    terminado en hacha2.c

--- p1/ej3/hacha2.c
+++ p1/ej3/hacha2.c
@@ -5,36 +5,49 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define BUF_SIZE 1024
 
-void crear_hijos(char *nombre, int fd,int bytes){
-    printf("Voy a crear un hijo \n");
-    if(fork() == 0){
-        printf("Entro al if \n");
-        char buffer;
-        int escribe;
-        int bytes_read;
-        escribe = open(nombre,O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
-        if(escribe == -1){
-            exit(EXIT_FAILURE);
+void crear_hijos(char archivo[], char *nombre, int fd[], int bytes, int *leidos){
+    //printf("Voy a crear un hijo \n");
+    char buffer[bytes];
 
-        }
-        do{
-            printf("Entro al do \n");
-            bytes_read = read(fd,&buffer,bytes);
-            printf("HE LEIDO %d \n", bytes_read);
-            write(escribe,&buffer,bytes_read);
+    int lee = open(archivo,O_RDONLY); //abrir el archivo
+    lseek(lee,*leidos,SEEK_CUR);
+    *leidos+=bytes;
 
-        }while(bytes_read == bytes);
-        printf("HE ACABAO \n");
-        close(escribe);
-        exit(EXIT_SUCCESS);
+
+    //printf("Nomobre del archivo a leer %s \n", archivo);
+    if(lee == -1){
+        exit(EXIT_FAILURE);
     }
+    printf("Creando archivo %s ...\n", nombre);
+
+    int bytes_read = read(lee,&buffer,bytes);
+    //printf("HE LEIDO %d \n", bytes_read);
+    int bytes_written = write(fd[1],&buffer,bytes_read);
+    //printf("HE ESCRITO %d \n", bytes_written);
+
+    if (fork()==0){
+        //printf("Entro al if \n");
+        int escribe = open(nombre,O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+        //printf("Open2 %d", escribe);
+        bytes_read = read(fd[0],&buffer,bytes);
+        //printf("HE LEIDO2 %d \n", bytes_read);
+        bytes_written = write(escribe,&buffer,bytes_read);
+        //printf("HE ESCRITO2 %d \n", bytes_written);
+        close(escribe);
+        kill(getpid(),SIGKILL);
+    }
+    
+    close(lee);
+    
 }
 
 void crear_nombres(int n_hijos, int hijos_creados, char *args, char *nombre){
-    printf("voy a crear un nombre \n");
+    //printf("voy a crear un nombre \n");
     char aux;
     int j;
     bool nombre_copiado = false;
@@ -58,8 +71,8 @@ void crear_nombres(int n_hijos, int hijos_creados, char *args, char *nombre){
     nombre[j]=aux;
     j++;
     nombre[j]='\0';
-    printf("NOMBRE: %s \n",nombre);
-    
+    //printf("NOMBRE: %s \n",nombre);
+
 }
 
 
@@ -67,7 +80,7 @@ void crear_nombres(int n_hijos, int hijos_creados, char *args, char *nombre){
 
 
 int main(int argc, char *argv[]){
-    int bytes, df, size_total, n_hijos = 0, hijos_creados = 0;
+    int bytes, df, size_total, n_hijos = 0, hijos_creados = 0, bytes_leidos = 0;
     struct stat sfile; //necesario para saber el tamaño de la canción
     char nombre_archivo[200];
     int bytes_read = 0, bytes_written = 0;
@@ -85,7 +98,7 @@ int main(int argc, char *argv[]){
 
 
     //Cálculo de cuantos hijos/archivos hay que crear
-    printf("llego a calcular cuantos hijos \n");
+    //printf("llego a calcular cuantos hijos \n");
     stat(argv[1], &sfile);
     size_total = sfile.st_size;
     if(size_total%bytes == 0){
@@ -95,34 +108,25 @@ int main(int argc, char *argv[]){
         n_hijos = (size_total/bytes) + 1;
     }
 
-    //Crear el array de pipes
-    int pipes[n_hijos*2];
-
     //bucle para crear los hijos
     for ( int i = 0; i < n_hijos;i++){
-        printf("%d \n",i);
+        int fd[2];
+        //printf("%d \n",i);
         
-        pipe(&pipes[2*i]); //crear el pipe que concecte el padre con ese hijo
-        close(pipes[2*i+1]);
+        pipe(fd); //crear el pipe que concecte el padre con ese hijo
+
         crear_nombres(n_hijos,hijos_creados,argv[1],nombre_archivo); //nombre_archivo = nombre del archivo creado por ese hijo
         hijos_creados++;
 
-        crear_hijos(nombre_archivo,pipes[2*i],bytes);
-        close(pipes[2*i]);
+        crear_hijos(argv[1],nombre_archivo,fd,bytes,&bytes_leidos);
+
+
+        close(fd[0]);
+
+        sleep(3); 
+
 
     }
-
-    do{
-        for(int i = 0; i < n_hijos; i++){
-          bytes_written = write(pipes[(2*i) + 1],&buffer,bytes_read);
-        }
-        
-    }while(bytes_written == bytes);
-
-
-
-
-
-
+    printf("Fin! \n");
 
 }
